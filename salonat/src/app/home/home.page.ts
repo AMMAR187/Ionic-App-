ecc1e357a08981f3680d3e95fcdb05fa1e35f302
Ammar Dafaalla Mahmoud <mr.ammar.96@gmail.com>
2019-08-20 13:17:55 +0530

    inital setup

---
+++ salonat/src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { TranslateConfigService } from '../providers/translate-config.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  selectedLanguage: string;
+  constructor(private translateConfigService: TranslateConfigService) {
+    this.selectedLanguage = this.translateConfigService.getDefaultLanguage();
+  }
+  languageChanged() {
+    this.translateConfigService.setLanguage(this.selectedLanguage);
+  }
+
+}
